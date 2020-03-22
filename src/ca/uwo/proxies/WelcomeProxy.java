37d9ea7cc86be41c37e262ecfb895ff66af70142
Connor Ciavarella <30648182+greenappleee@users.noreply.github.com>
2020-03-22 19:19:25 -0400

    closed input again

--- src/ca/uwo/proxies/WelcomeProxy.java
+++ src/ca/uwo/proxies/WelcomeProxy.java
@@ -74,6 +74,8 @@ public class WelcomeProxy extends Proxy {
 		System.out.println("Enter your password:");
 		String password = user_input.nextLine();
 		
+		user_input.close();
+		
 		if((username.equals(buyer.getUserName())) && (password.equals(buyer.getPassword()))) {
 			
 			System.out.println("Authenticated.");
